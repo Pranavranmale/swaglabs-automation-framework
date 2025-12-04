1edc6072349b83fa0f46300a6e97b00d7ea853df
Pranavranmale <pranavranmale1010@gmail.com>
2025-12-04 11:45:27 +0530

    adding cart functionality

--- PageObject/Cart.ts
+++ PageObject/Cart.ts
@@ -11,4 +11,21 @@ export class Cart{
         await this.page.locator(".shopping_cart_container").click()
         await this.page.locator('#checkout').click();
     }
+     async formdetails(){
+        await this.page.getByPlaceholder('First Name').fill('John');
+        await this.page.getByPlaceholder('Last Name').fill('Doe');
+        await this.page.getByPlaceholder('Zip/Postal Code').fill('12345');
+        await this.page.locator('#continue').click();
+    }
+    async checkout(){
+        await this.page.getByText('Checkout: Overview').isVisible();
+        await this.page.getByText('Sauce Labs Backpack').isVisible();
+        await this.page.getByText('Total: $32.39').isVisible();
+        await this.page.getByText('Payment Information:').isVisible();
+        await this.page.getByText('Shipping Information:').isVisible();
+        await this.page.getByText('Price Total').isVisible();
+        await this
+        await this.page.locator('#finish').click();
+        await this.page.getByText('THANK YOU FOR YOUR ORDER').isVisible();
+    }   
   }
--- tests/AllTest.spec.ts
+++ tests/AllTest.spec.ts
@@ -2,7 +2,7 @@ import {test,expect} from '@playwright/test'
 import { Login } from '../PageObject/login'
 import { DropDown } from '../PageObject/DropDown'
 import {AddProdunct} from '../PageObject/addProduct'
-import { Cart } from '../PageObject/Cart'
+import { Cart } from '../PageObject/Cart'    
 test.beforeEach('Login',async({page})=>{
     await page.goto('https://www.saucedemo.com/') 
     const login=new Login(page)
@@ -14,18 +14,18 @@ test.describe('All Test',async()=>{
         await dropdown.dropdown()
         await page.waitForTimeout(2000);
         
-    })
+    });
     test('select product',async({page})=>{
         const product = new AddProdunct(page)
         await product.AddProdunct()
         await page.waitForTimeout(2000);
-    })
+    });
 
     test('Cart Functionality',async({page})=>{
         const cart = new Cart(page)
         await cart.AddProdunct()
         await page.waitForTimeout(2000);
-    })
-    
-    
-});
+        await cart.formdetails()
+        await page.waitForTimeout(2000);
+    });
+})
