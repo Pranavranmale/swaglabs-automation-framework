15a4e22ab9696cce32ae3965c744e86db24b397a
Pranavranmale <pranavranmale1010@gmail.com>
2025-12-07 23:22:51 +0530

    remove old testname file

--- src/utils/AllTestNames.ts
+++
@@ -1,7 +0,0 @@
-export const TestNames={
-AddProductName:"Add product to cart and verify Products page",
-E2ETestName:"Complete purchase flow",
-CheckoutFlowName:"Cart to Checkout Flow",
-LoginTestName:"Login Test",
-DropDownTestName:"Validate Product and Sorting"
-}
